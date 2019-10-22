a3cd9f422c6873654352df359e013ed0177daed1
Levy Klots <levyklots@gmail.com>
2019-10-21 17:58:55 -0700

    implement dummy question query

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type Query {
+    question: Question
+  }
+
+  type Question {
+    id: ID!
+    pic: String!
+    choices: [Choice]!
+  }
+
+  type Choice {
+    id: ID!
+    name: String!
+    commonName: String!
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  question: () => {
+    const q = {
+      id: 'testid',
+      pic: 'https://static.inaturalist.org/photos/3235960/medium.jpg?1459121239',
+      choices: [{
+        id: 47347,
+        name: 'Cantharellus cibarius',
+        commonName: 'Golden Chanterelle',
+      },
+      {
+        id: 67752,
+        name: 'Omphalotus olivascens',
+        commonName: 'Western American Jack-o\'-lantern Mushroom',
+      }],
+    };
+    return q;
+  },
+};
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
