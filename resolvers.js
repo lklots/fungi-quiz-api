c50aa6248585f89ae5a09b701920d87acea84745
Levy Klots <levyklots@gmail.com>
2019-11-09 08:37:52 +0500

    initial working version of createQuiz

--- resolvers.js
+++ resolvers.js
@@ -3,6 +3,19 @@ const axios = require('axios');
 
 const INAT_API = 'https://api.inaturalist.org/v1';
 
+function makeQuestion(obs, choices) {
+  const photos = obs.photos.map((photo) => ({
+    url: photo.url.replace('square.jp', 'medium.jp'),
+    origWidth: photo.original_dimensions.width,
+    origHeight: photo.original_dimensions.height,
+  }));
+  return {
+    questionId: obs.id,
+    photos,
+    choices,
+  };
+}
+
 async function createQuiz({ taxonIds }) {
   const observations = await axios({
     method: 'get',
@@ -15,35 +28,20 @@ async function createQuiz({ taxonIds }) {
       order_by: 'created_at',
     },
   });
-
-  // pick a random observation
-  const obs = _.sample(observations.data.results.filter((x) => x.photos.length >= 2));
-  const photos = obs.photos.map((photo) => ({
-    url: photo.url.replace('square.jp', 'original.jp'),
-    origWidth: photo.original_dimensions.width,
-    origHeight: photo.original_dimensions.height,
-  }));
+  const obsWithPhotos = observations.data.results.filter((x) => x.photos.length >= 2);
+  const groupByTaxonId = _.groupBy(obsWithPhotos, (x) => x.taxon.id);
+  const choices = _.reduce(groupByTaxonId, (obj, v) => ({
+    ...obj,
+    [v[0].taxon.id]: {
+      taxonId: v[0].taxon.id,
+      name: v[0].taxon.name,
+      commonName: v[0].taxon.preferred_common_name,
+    },
+  }), {});
+  const picks = _.flatten(_.map(groupByTaxonId, (v) => _.sampleSize(v, _.random(3, 5))));
   return {
     quizId: 'quizId',
-    questions: [{
-      questionId: 'testid',
-      photos,
-      choices: [{
-        taxonId: obs.taxon.id,
-        name: obs.taxon.name,
-        commonName: obs.taxon.preferred_common_name,
-      },
-      {
-        taxonId: 67752,
-        name: 'Omphalotus olivascens',
-        commonName: 'Western American Jack-o\'-lantern Mushroom',
-      },
-      {
-        taxonId: 63538,
-        name: 'Hygrophoropsis aurantiaca',
-        commonName: 'False Chanterelle',
-      }],
-    }],
+    questions: picks.map((p) => makeQuestion(p, _.values(choices))),
   };
 }
 
